f5f8044f74679cf414a4fbf171f51042973be76a
Gilbert Buchner <gilbert@familie-buchner.at>
2024-01-03 21:00:19 +0100

    first complete configuration

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim"
+        },
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                },
+            },
+        },
+        config = function()
+            vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+        end
+
+    }
+}
